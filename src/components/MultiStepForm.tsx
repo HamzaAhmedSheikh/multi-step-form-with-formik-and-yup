f80e96390abc1249c8f2366418a9b642901cce05
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-25 02:56:20 +0500

    update

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -1,30 +1,51 @@
 import React from 'react';
 import { Card, CardContent } from '@material-ui/core';
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik';
 import { CheckboxWithLabel, TextField } from 'formik-material-ui';
-
+import { object, mixed, number } from 'yup';
 
 export default function Home () {
   return (
     <Card>
       <CardContent>
-       <Formik initialValues={{
-           firstName: "",
-           lastName: "",
-           millionaire: false,
-           money: 0,
-           description: "",
+       <Formik
+         validationSchema={object({
+           money: mixed().when('millionaire', {
+            is: true,
+            then: number().required().min(1_000_000, 'Because you said you are a millionaire you need to have 1 million.'),
+            otherwise: number().required()   
+           })  
+         })}
+         initialValues={{
+            firstName: "",
+            lastName: "",
+            millionaire: false,
+            money: 0,
+            description: "",
 
        }} onSubmit={() => {}}>
         <Form autoComplete="off">
-          <Field name='firstName' component={TextField} label="First Name" />
-          <Field name='lastName' component={TextField} label="Last Name" />
-          <Field name='millionaire' type="checkbox" component={CheckboxWithLabel} Label={{ label: "I am a millionaire"  }} />
-          <Field name='money' type="number" component={TextField} label="All the money I have" />
-          <Field name='description' component={TextField} label="Description" />              
+          <div>
+            <Field name='firstName' component={TextField} label="First Name" />
+            <Field name='lastName' component={TextField} label="Last Name" />
+            <Field name='millionaire' type="checkbox" component={CheckboxWithLabel} Label={{ label: "I am a millionaire"  }} />
+          </div> 
+
+          <div>
+            <Field name='money' type="number" component={TextField} label="All the money I have" />
+          </div>
+
+          <div>          
+           <Field name='description' component={TextField} label="Description" />              
+          </div> 
         </Form>  
        </Formik>    
       </CardContent> 
     </Card>  
   )  
+}
+
+
+export function FormikStepper({children, ...props} : FormikConfig<FormikValues>) {
+
 }
