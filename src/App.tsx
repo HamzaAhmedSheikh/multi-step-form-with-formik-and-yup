d72abc25a8822a11329f733c8a3733b791f59cf1
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-03 06:42:44 +0500

    I have styled the multistep form in material ui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { AppBar, Box, Container, CssBaseline, ThemeProvider, Toolbar, Typography } from '@material-ui/core';
+import { theme } from './theme';
 import './App.css';
 import Home from './components/MultiStepForm';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+     <ThemeProvider theme={theme}>
+      <AppBar position="fixed">
+       <Toolbar variant="dense">
+        <Typography variant="h6"> Multi-Step Form </Typography>
+       </Toolbar>
+      </AppBar>
+
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Container>
+          <Box marginTop={10}>
+            <Home />
+          </Box>
+        </Container>
+     </ThemeProvider> 
+    </>
   );
 }
 
